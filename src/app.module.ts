3de27e252b323af0d76df707a50b5cfef84e6b85
rayzen2002 <emanuelzsmj@gmail.com>
2025-09-27 22:24:30 -0300

    Fix: Limpando arquivos não usados(gerados pelo nest)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
@@ -8,7 +6,5 @@ import { CartModule } from './cart/cart.module';
 
 @Module({
   imports: [UsersModule, AuthModule, ProductsModule, CartModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
