6c4caba3203a8a004e6a9ee78624c4f999f05907
rayzen2002 <emanuelzsmj@gmail.com>
2025-09-28 00:13:45 -0300

    Feat(auth): Adicionando verificacao de is_active para controle das rotas autenticadas

--- src/auth/jwt.guard.ts
+++ src/auth/jwt.guard.ts
@@ -4,17 +4,22 @@ import {
   Injectable,
   ForbiddenException,
 } from '@nestjs/common';
+import type { InferSelectModel } from 'drizzle-orm';
+import type { users } from 'src/db/schema';
 
+export type User = InferSelectModel<typeof users>;
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private readonly allowedRoles: string[]) {}
 
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
-    const user = request.user;
-
+    const user : User = request.user;
     if (!user) {
       throw new ForbiddenException('Usuário não autenticado');
+    }
+    if(user.is_active !== true){
+      throw new ForbiddenException('Conta não ativada. Verifique seu email para ativar a conta')
     }
 
     if (!this.allowedRoles.includes(user.role)) {
